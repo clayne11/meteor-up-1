4b5fab0d94bb263b8ad4c69cfac65ab9446f6055
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-05-18 18:40:07 +0300

    Make mongo version configurable

--- scripts/linux/install-mongodb.sh
+++ scripts/linux/install-mongodb.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+MONGO_VERSION=<%= mongoVersion %>
 
 set -e
 # we use this data directory for the backward compatibility
 # older mup uses mongodb from apt-get and they used this data directory
 sudo mkdir -p /var/lib/mongodb
 
-sudo docker pull mongo:latest
+sudo docker pull mongo:$MONGO_VERSION
 set +e
 sudo docker rm -f mongodb
 set -e
